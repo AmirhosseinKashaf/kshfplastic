e5b0be1d004d564e65870231b4f91044c3efc2c5
AmirhosseinKashaf <kashafamirhossein@gmail.com>
2025-03-21 21:35:21 +0330

    single blog and template tags fixed

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,24 @@
+from django import template
+from blog.models import Post,Category,Comment
+from django.utils import timezone
+
+
+register = template.Library()
+
+@register.inclusion_tag('blog/blog-latest-posts.html')
+def latestposts(args=3):
+   posts = Post.objects.filter(published_date__lte=timezone.now(),status=1)[:args]
+   return {'posts':posts}
+
+@register.simple_tag(name='comments_count')
+def function(pid):   
+   return Comment.objects.filter(post=pid,approved=True).count()
+
+@register.inclusion_tag('blog/blog-categories.html')
+def postcategories():
+   posts = Post.objects.filter(published_date__lte=timezone.now(),status=1)
+   categories = Category.objects.all()
+   cat_dict = {}
+   for name in categories:
+      cat_dict[name] = posts.filter(category=name).count()
+   return {'categories':cat_dict}
--- kshfplast/settings.py
+++ kshfplast/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website',
     'blog',
+    'django.contrib.humanize',
     'django_summernote',
     
 ]
